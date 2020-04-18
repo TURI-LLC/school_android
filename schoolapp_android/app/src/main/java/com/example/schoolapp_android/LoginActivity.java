0dd977ef7a23d6ed5f0e91e2a4c038a42a473db9
kunkunnmsl <49770123+kunkunnmsl@users.noreply.github.com>
2020-04-18 11:14:29 +0800

    更改依赖添加依赖

--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/LoginActivity.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/LoginActivity.java
@@ -28,6 +28,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         txt_usr = (EditText)findViewById(R.id.txt_user);
+        til_usr = (TextInputLayout)findViewById(R.id.til_pwd);
+        til_pwd = (TextInputLayout)findViewById(R.id.til_pwd);
+        txt_pwd = (EditText)findViewById(R.id.txt_pwd);
+        con_prg = (ProgressBar)findViewById(R.id.connection_prg);
         txt_usr.addTextChangedListener(new TextWatcher(){   //设置监听器
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -39,9 +43,13 @@ public class LoginActivity extends AppCompatActivity {
                 btn_cls = (ImageButton)findViewById(R.id.btn_clr1);
                 if(!txt_usr.getText().toString().isEmpty()){
                     btn_cls.setVisibility(View.VISIBLE);
+                    til_pwd.setVisibility(View.VISIBLE);
                 } else {
                     btn_cls.setVisibility((View.INVISIBLE));
+                    til_pwd.setVisibility(View.GONE);
                 }
+
+
             }
 
             @Override
@@ -66,29 +74,26 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     public void btnNext_onClick(View view) {    //下一步按钮
-        til_usr = (TextInputLayout)findViewById(R.id.til_pwd);
-        til_pwd = (TextInputLayout)findViewById(R.id.til_pwd);
-        txt_pwd = (EditText)findViewById(R.id.txt_pwd);
-        con_prg = (ProgressBar)findViewById(R.id.connection_prg);
+
 
         con_prg.setVisibility(View.VISIBLE);
         txt_usr.setText(txt_usr.getText().toString().trim());   //去除空格
-        if(txt_usr.getText().toString().isEmpty()){
-            //TODO:如果用户没有输入,屏幕下方弹出警告
-        }else{
-            //TODO:如果有用户输入,开始查找用户
-            txt_usr.setEnabled(false);//通讯时禁用用户名输入框
-            btn_cls.setEnabled(false);//通讯时禁用清除按钮
-            if(true){  //TODO:补充判断条件(查找数据库中学号,手机号,邮箱字段)
-                til_pwd.setVisibility(View.VISIBLE);    //显示密码框
-            }else{
-                //TODO:如果未找到用户,询问是否要创建用户
-
-                txt_usr.setEnabled(true);   //开放用户名输入框
-            }
-        }
-        btn_cls.setEnabled(true); //通讯完成后启用清除按钮
-        con_prg.setVisibility(View.GONE);   //通讯完成后隐藏进度条
+//        if(txt_usr.getText().toString().isEmpty()){
+//            //TODO:如果用户没有输入,屏幕下方弹出警告
+//        }else{
+//            //TODO:如果有用户输入,开始查找用户
+//            txt_usr.setEnabled(false);//通讯时禁用用户名输入框
+//            btn_cls.setEnabled(false);//通讯时禁用清除按钮
+//            if(true){  //TODO:补充判断条件(查找数据库中学号,手机号,邮箱字段)
+//                til_pwd.setVisibility(View.VISIBLE);    //显示密码框
+//            }else{
+//                //TODO:如果未找到用户,询问是否要创建用户
+//
+//                txt_usr.setEnabled(true);   //开放用户名输入框
+//            }
+//        }
+//        btn_cls.setEnabled(true); //通讯完成后启用清除按钮
+//        con_prg.setVisibility(View.GONE);   //通讯完成后隐藏进度条
     }
     public void btnReg_onClick(View view){
         //跳转到注册页面
