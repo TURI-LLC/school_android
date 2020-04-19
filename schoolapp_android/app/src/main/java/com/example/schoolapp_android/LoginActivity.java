dd22278750b9d18b46df3f5b116d4aef1aaba64e
RA1N <RA1NN1AR@users.noreply.github.com>
2020-04-19 09:58:28 +0800

    代码优化,新增底部消息框

--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/LoginActivity.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/LoginActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
@@ -34,6 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         txt_usr = (EditText)findViewById(R.id.txt_user);    //用户名输入框
         btn_next = (ImageButton)findViewById(R.id.btn_next);    //下一步按钮
+        btn_next.setClickable(false); //副视图的xml不知为何属性设置无效,故在此再次声明属性.
         con_prg = (ProgressBar)findViewById(R.id.connection_prg);   //进度条动画
         txt_pwd = (EditText)findViewById(R.id.txt_pwd); //密码输入框
         til_usr = (TextInputLayout)findViewById(R.id.til_pwd);  //用户名输入框架
@@ -41,10 +44,7 @@ public class LoginActivity extends AppCompatActivity {
 
         txt_usr.addTextChangedListener(new TextWatcher(){   //设置用户文本框监听器
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count){
                 btn_cls = (ImageButton)findViewById(R.id.btn_clr1);
@@ -57,13 +57,15 @@ public class LoginActivity extends AppCompatActivity {
                     btn_next.setClickable(true);
                     btn_next.setBackground(getDrawable(R.drawable.btn_next));
                 }
-
-
             }
-
             @Override
-            public void afterTextChanged(Editable s) {
-
+            public void afterTextChanged(Editable s) {}
+        });
+        txt_usr.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                btnNext_onClick(findViewById(R.id.login_view));  //当按下软键盘的完成按钮时,调用下一步按钮单击事件
+                return true;
             }
         });
 
@@ -76,13 +78,13 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void btnClr_onClick(View view){  //清除按钮
+    public void btnClr_onClick(View view){  //清除按钮单击事件
         txt_usr.setText("");
         til_pwd.setVisibility(View.GONE);
         txt_usr.setEnabled(true);
 
     }
-    public void btnNext_onClick(View view) {    //下一步按钮
+    public void btnNext_onClick(View view) {    //下一步按钮单击事件
         btn_next.setClickable(false); btn_next.setBackground(getDrawable(R.drawable.btn_next_gray));    //暂时禁用登录按钮
         con_prg.setVisibility(View.VISIBLE);    //显示进度条,提示用户正在查找
         txt_usr.setText(txt_usr.getText().toString().trim());   //去除空格
@@ -90,12 +92,19 @@ public class LoginActivity extends AppCompatActivity {
             txt_pwd.setText("");    //清空上一次输入的密码
             if(!txt_usr.getText().toString().isEmpty()){
                 //如果用户名输入框有内容,开始通讯
-                txt_usr.setEnabled(false);//通讯时禁用用户名输入框
-                btn_cls.setEnabled(false);//通讯时禁用清除按钮
-                if(true){  //TODO:查找数据库中用户名,手机号,邮箱字段
+                txt_usr.setEnabled(false);  //通讯时禁用用户名输入框
+                btn_cls.setEnabled(false);  //通讯时禁用清除按钮
+                if(false){  //TODO:查找数据库中用户名,手机号,邮箱字段
                     til_pwd.setVisibility(View.VISIBLE);    //显示密码框
                 }else{
-                    //TODO:提示未找到用户
+                    Snackbar.make(view,"我们未能查找到这个用户,如果您是新用户,请单击\"注册\"按钮.",Snackbar.LENGTH_SHORT)
+                            .setAction("注册",new View.OnClickListener(){
+                                @Override
+                                public void onClick(View v){
+                                    btnReg_onClick(v);
+                                }
+                            })
+                            .show();
                     txt_usr.setEnabled(true);   //开放用户名输入框
                     //TODO:显示圆形注册按钮
                 }
@@ -114,7 +123,7 @@ public class LoginActivity extends AppCompatActivity {
         btn_next.setClickable(true);
         btn_next.setBackground(getDrawable(R.drawable.btn_next));
     }
-    public void btnReg_onClick(View view){
+    public void btnReg_onClick(View view){  //注册按钮单击事件
         //跳转到注册页面
         Intent go2reg = new Intent(this,RegActivity.class);
         go2reg.putExtra("username",txt_usr.getText().toString());   //自动为用户填充注册UI的用户名(从登录UI
--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/RegActivity.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/RegActivity.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class RegActivity extends AppCompatActivity {
     private EditText txt_name;
     private EditText txt_pwd;
@@ -49,19 +51,55 @@ public class RegActivity extends AppCompatActivity {
     }
 
     public void btnSubmit_onClick(View view){
-        dialog_cfm();
+        //TODO:检查表单内容格式,通过后显示确认对话框,不通过则告知用户重新输入.
+        if(true){go_dialog();}
+        else{}
     }
-
-    public void dialog_cfm(){
-        boolean conditionA = !txt_email.getText().toString().isEmpty(), conditionB=!txt_phone.getText().toString().isEmpty();
+    public void go_dialog(){
+        //输出确认注册信息至对话框
+        boolean conditionA = txt_email.getText().toString().isEmpty(), conditionB=txt_phone.getText().toString().isEmpty();
         String context = "您的用户名: " + txt_name.getText().toString().trim();
         context+="\n您的密码: " + txt_name.getText().toString();
-        if(conditionA){ context +="\n您的邮件地址: "+txt_email.getText().toString(); }
-        if(conditionB){ context +="\n您的手机号码: "+txt_phone.getText().toString(); }
+        if(!conditionA){ context +="\n您的邮件地址: "+txt_email.getText().toString(); }
+        if(!conditionB){ context +="\n您的手机号码: "+txt_phone.getText().toString(); }
         context+="\n________________________________________";
-        if(conditionA==conditionB){ context+="\n\n* 请注意:您没有提供任何额外信息,这会降低找回账号的成功率."; }
+        if(conditionA&&conditionB){ context+="\n\n* 请注意:您没有提供任何额外信息,这会降低找回账号的成功率."; }
         context +="\n\n"+getString(R.string.need_choose);
+        //调用确认对话框
+        confirmDialog(context);
+    }
+    public void textWatchR(){   //文本框监视,由文本框监听器进行外部调用.
+        if(txt_pwd.getText().toString().isEmpty()||txt_name.getText().toString().trim().isEmpty()){
+            //如果用户和密码输入任意其一没有内容,禁用提交按钮
+            btn_submit.setClickable(false);
+            btn_submit.setBackground(getDrawable(R.drawable.btn_next_gray));
+        } else {
+            //如果有内容,启用提交按钮
+            btn_submit.setClickable(true);
+            btn_submit.setBackground(getDrawable(R.drawable.btn_next));
+        }
+    }
+    private void start_reg(){   //注册
+        ProgressDialog loading = new ProgressDialog(RegActivity.this);
+        loading.setTitle("正在注册...");
+        loading.setMessage("请稍等,\n正在与服务器通讯.");
+        loading.setIndeterminate(true);
+        loading.setCancelable(false);
+        loading.show();
 
+        //TODO:向服务器数据库用户表添加一条记录(要将邮件字符串toLowerCase();)
+        if(true){   //如果注册成功,跳转至主界面
+            Intent go2main = new Intent(this,MainActivity.class);
+            go2main.putExtra("username",txt_name.getText().toString());  //为主界面传送用户信息
+            startActivity(go2main);
+        }else{
+            snackBar_err(findViewById(R.id.reg_view));  //向用户提供错误信息
+        }
+
+        loading.dismiss();
+
+    }
+    private void confirmDialog(String context){     //确认对话框
         final AlertDialog.Builder confirmDialog = new AlertDialog.Builder(RegActivity.this);
         confirmDialog.setIcon(R.drawable.ic_info);
         confirmDialog.setTitle(getString(R.string.request_confirm));
@@ -78,33 +116,24 @@ public class RegActivity extends AppCompatActivity {
         });
         confirmDialog.show();
     }
-    public void textWatchR(){   //文本框监视
-        if(txt_pwd.getText().toString().isEmpty()||txt_name.getText().toString().trim().isEmpty()){ //如果用户和密码输入框其中任一没有内容,禁用提交按钮
-            btn_submit.setClickable(false);
-            btn_submit.setBackground(getDrawable(R.drawable.btn_next_gray));
-        } else {    //如果有内容,启用提交按钮
-            btn_submit.setClickable(true);
-            btn_submit.setBackground(getDrawable(R.drawable.btn_next));
-        }
+    private void snackBar_err(View view){   //错误消息栏
+        String err_msg = "发生错误,请稍后再试.";
+        Snackbar.make(view,err_msg,Snackbar.LENGTH_SHORT)
+                .setAction("重置",new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        reset();
+                    }
+                })
+                .show();
     }
-    private void start_reg(){   //注册
-        ProgressDialog loading = new ProgressDialog(RegActivity.this);
-        loading.setTitle("正在注册...");
-        loading.setMessage("请稍等,\n正在与服务器通讯.");
-        loading.setIndeterminate(true);
-        loading.setCancelable(false);
-        loading.show();
-
-        //TODO:向服务器用户表添加一条记录(要将邮件字符串toLowerCase();)
-        if(true){   //如果注册成功,跳转至主界面
-            Intent go2main = new Intent(this,MainActivity.class);
-            go2main.putExtra("username",txt_name.getText().toString());  //为主界面传送用户信息
-            startActivity(go2main);
-        }else{
-            //TODO:向用户提供错误信息
-        }
-
-        loading.dismiss();
-        //TODO:清空所有文本框,以备下一次注册.
+    public void reset(){    //重置
+//        txt_name.setText("");
+//        txt_pwd.setText("");
+//        txt_email.setText("");
+//        txt_phone.setText("");
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
