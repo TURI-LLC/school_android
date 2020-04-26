b80ebf20e281c0c15da8f474fc1756ed4c182543
RA1N <RA1NN1AR@users.noreply.github.com>
2020-04-26 09:54:47 +0800

    设置UI完成

---
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/SettingsActivity.java
@@ -0,0 +1,42 @@
+package com.example.schoolapp_android;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+    Button btn_Logout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        btn_Logout = findViewById(R.id.btn_logout);
+    }
+    void btnLogout_Click(View v){
+        final AlertDialog.Builder logoutDialog = new AlertDialog.Builder(SettingsActivity.this);
+        logoutDialog.setIcon(R.drawable.ic_warning);
+        logoutDialog.setTitle("警告");
+        logoutDialog.setMessage("您确定要退出吗?\n您的账号信息仍会保留,但会被要求重新登录.");
+        logoutDialog.setPositiveButton("登出", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //反向登录工程
+                Intent logout = new Intent(SettingsActivity.this,LoginActivity.class);
+                SharedPreferences sharedPreferences = getSharedPreferences("settings", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("isLogin",false);
+                editor.putString("username",null);
+                editor.putString("pwd",null);
+                startActivity(logout);
+                finish();
+            }
+        });
+    }
+}
--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/fragment/MineFragment.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/fragment/MineFragment.java
@@ -1,5 +1,7 @@
 package com.example.schoolapp_android.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -9,11 +11,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.schoolapp_android.MainActivity;
 import com.example.schoolapp_android.R;
+import com.example.schoolapp_android.SettingsActivity;
 
 import htmlservice.check_kick;
 import javabean.JavaBean;
@@ -36,60 +41,61 @@ public class MineFragment extends Fragment {
     TextView kick;
     TextView sno;
     TextView school;
+    Button btn_Settings;
+
     public MineFragment() {
         // Required empty public constructor
     }
-
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);;
+        init();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        Bundle bundle =this.getArguments();
-        user=bundle.getString("username");
-        pwd=bundle.getString("pwd");
-
+        Bundle bundle = this.getArguments();
+        user = bundle.getString("username");
+        pwd = bundle.getString("pwd");
         view = inflater.inflate(R.layout.fragment_mine, container, false);
-        school=view.findViewById(R.id.user_school);
-        sno=view.findViewById(R.id.user_sno);
-        kick=view.findViewById(R.id.user_kick);
-        img=view.findViewById(R.id.user_avatar);
+        school = view.findViewById(R.id.user_school);
+        sno = view.findViewById(R.id.user_sno);
+        kick = view.findViewById(R.id.user_kick);
+        img = view.findViewById(R.id.user_avatar);
         new checkkick().execute();
         return view;
     }
 
+    private class checkkick extends AsyncTask<Void, String, ArrayList<JavaBean>> {
+        private String url = "http://xxschoolapp.oss-cn-beijing.aliyuncs.com/img/";
 
-
-
-
-
-    private  class checkkick extends AsyncTask<Void,String, ArrayList<JavaBean>> {
-        private String url="http://xxschoolapp.oss-cn-beijing.aliyuncs.com/img/";
         @Override
         protected ArrayList<JavaBean> doInBackground(Void... voids) {
-            check_kick a =new check_kick();
+            check_kick a = new check_kick();
 
-            return  a.check_kick(user,pwd);
+            return a.check_kick(user, pwd);
         }
 
         @Override
         protected void onPostExecute(ArrayList<JavaBean> strings) {
             super.onPostExecute(strings);
-            userlist=strings;
+            userlist = strings;
 
             school.setText(userlist.get(0).U_school);
             kick.setText(userlist.get(0).U_kick);
-            String snos=userlist.get(0).U_sno;
-            if(snos!=null){
+            String snos = userlist.get(0).U_sno;
+            if (snos != null) {
                 sno.setText(snos);
-            }else{
+            } else {
                 sno.setText("学号未设定");
             }
 
-            new load_image(img).execute(url+userlist.get(0).U_img);
+            new load_image(img).execute(url + userlist.get(0).U_img);
         }
     }
+
     Drawable LoadImageFromWebOperations(String url) {
         InputStream is = null;
         Drawable d = null;
@@ -103,12 +109,10 @@ public class MineFragment extends Fragment {
     }
 
     private class load_image extends AsyncTask<String, Void, Drawable> {
-
         ImageView img;
 
         load_image(ImageView img) {
             // list all the parameters like in normal class define
-
             this.img = img;
         }
 
@@ -117,12 +121,22 @@ public class MineFragment extends Fragment {
             Drawable drawable = LoadImageFromWebOperations(params[0]);
             return drawable;
         }
+
         @Override
         protected void onPostExecute(Drawable result) {
             super.onPostExecute(result);
 
             img.setImageDrawable(result);
         }
-
+    }
+    private void init(){
+        btn_Settings=(Button)getActivity().findViewById(R.id.btn_goSettings);
+        btn_Settings.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent goSettings = new Intent(getActivity(), SettingsActivity.class);
+                startActivity(goSettings);
+            }
+        });
     }
 }
