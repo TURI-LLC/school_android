e96a75549a14091c4454bfd1e29deae7427cace7
kunkunnmsl <49770123+kunkunnmsl@users.noreply.github.com>
2020-04-27 23:59:48 +0800

    4-27二次更改完善课程表

--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/Son/courseDetailActivity.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/Son/courseDetailActivity.java
@@ -3,13 +3,23 @@ package com.example.schoolapp_android.Son;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.schoolapp_android.R;
 
-public class courseDetailActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class courseDetailActivity extends AppCompatActivity {
+    private TextView teacher;
+    private TextView classname;
+    private TextView address;
+    private TextView week;
+    private TextView jie;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +30,24 @@ public class courseDetailActivity extends AppCompatActivity {
             actionBar.setHomeButtonEnabled(true);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        Intent intent =getIntent();
+        ArrayList<String> s=new ArrayList<>();
+        s= intent.getStringArrayListExtra("xinxi");
+        System.out.println("你得到了"+s.size());
+        teacher=findViewById(R.id.course_teacher);
+        classname=findViewById(R.id.course_classname);
+        address=findViewById(R.id.course_class);
+        week=findViewById(R.id.course_week);
+        jie=findViewById(R.id.course_jie);//
+
+        teacher.setText(s.get(0));
+        classname.setText(s.get(1));
+        week.setText("第"+s.get(2)+"周");
+        jie.setText(s.get(3)+"—"+s.get(4)+"节");
+        address.setText(s.get(5));
+
+
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
--- schoolapp_android/app/src/main/java/com/example/schoolapp_android/Son/kebiao_activity.java
+++ schoolapp_android/app/src/main/java/com/example/schoolapp_android/Son/kebiao_activity.java
@@ -181,11 +181,19 @@ public class kebiao_activity extends AppCompatActivity {
             shuabiao();
             String address="";
             String classname="";
+            String teacher="";
             Pattern p=Pattern.compile("[^0-9]");
             Matcher m = p.matcher(zhouci);
             zhouci =m.replaceAll("").trim();
             for(int n=0;n<kebiao.size();n++) {
                 if(kebiao.get(n).CoA_Semester.equals(xueqi)&&kebiao.get(n).CoA_weekly.equals(zhouci)){
+                    //获取老师
+                    for(int i=0;i<kecheng.size();i++){
+
+                        if(kecheng.get(i).Coi_id.equals(kebiao.get(n).Coi_id)){
+                            teacher=kecheng.get(i).Coi_teacher;
+                        }
+                    }
                     //获取第几周的课
                     if(kebiao.get(n).CoA_week==null){
                         for(int i=0;i<kecheng.size();i++){
@@ -225,6 +233,8 @@ public class kebiao_activity extends AppCompatActivity {
                         }
                     }
                     //规划课程名
+                    final ArrayList<String> xinxi=new ArrayList<>();
+                    xinxi.add(classname);
                     if(classname.length()>6){
                         classname=classname.substring(0,5)+"...";
                     }
@@ -241,12 +251,23 @@ public class kebiao_activity extends AppCompatActivity {
                     }
                     System.out.println(week+":"+start+":"+end);
                     boolean bol=true;
+
+
+
+                          xinxi.add(teacher);
+                          xinxi.add(classname);
+                          xinxi.add(String.valueOf(week));
+                          xinxi.add(String.valueOf(start));
+                          xinxi.add(String.valueOf(end));
+                          xinxi.add(address);
+
+
                     //第一个值为————行  第二个值为————列
 //
                     for (int i = 0;i<7; i++) { //列
                         for (int j = 0;j<tableLayout.getChildCount(); j++) {//行
                             if (i==week&&j>=start&&j<=end){
-                                int c= j*8+i+1;
+                                final int c= j*8+i+1;
                                 if(bol){
                                     TextView textView;
                                     TextView textView1;
@@ -268,6 +289,14 @@ public class kebiao_activity extends AppCompatActivity {
                                 }
 
                                 ((TableRow) tableLayout.getChildAt(j)).getChildAt(i).setBackgroundColor(Color.rgb(0, 150, 255));
+                                ((TableRow) tableLayout.getChildAt(j)).getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent =new Intent(kebiao_activity.this,courseDetailActivity.class);
+                                        intent.putStringArrayListExtra("xinxi",xinxi);
+                                        startActivity(intent);
+                                    }
+                                });
                             }
 
 
@@ -286,6 +315,7 @@ public class kebiao_activity extends AppCompatActivity {
             tableLayout = (TableLayout) findViewById(R.id.table1);
             tableLayout.removeAllViews();
             tableLayout.setStretchAllColumns(true);
+
             int ci=0;
             for (int i = 1; i <= jie; i++) {
 
@@ -297,6 +327,13 @@ public class kebiao_activity extends AppCompatActivity {
                     text.setHeight(150);
                     text.setWidth(50);
                     text.setId(ci);
+//                    text.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            System.out.println(v.getId());
+//                        }
+//                    });
+
 
                     if (j == 1 && i != 1) {
                         text.setText(String.valueOf(i - 1));
@@ -308,5 +345,10 @@ public class kebiao_activity extends AppCompatActivity {
                 //新建的TableRow添加到TableLayout
                 tableLayout.addView(tableRow, new TableLayout.LayoutParams(WC, MP,1));
             }
+
+
+
+
+
         }
     }
